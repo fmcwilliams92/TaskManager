345fe232a27192dfd40f293b9200102ea7798dfa
Fred McWilliams <fredmcwilliams92@gmail.com>
2022-06-25 21:23:05 -0500

    restructured the function to be more dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var buttonE1 = document.querySelector("#save-task");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-buttonE1.addEventListener("click", function() {
+var createTaskHandler = function() {
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
-  listItemE1.textContent = "This is my new task";
+  listItemE1.textContent = "This is a new task";
   tasksToDoE1.appendChild(listItemE1);
-});
+}
+
+buttonE1.addEventListener("click", createTaskHandler);
