0490e3501c678e50683dd8f9b820968dc7861ce4
Fredric McWilliams <fredmcwilliams92@gmail.com>
2022-06-27 17:20:44 -0500

    xit quit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
-var taskNameInput = document.querySelector("input[name='task-name']");
+
+// console.log(taskNameInput);
 
 var createTaskHandler = function(event) {
-
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  
   event.preventDefault();
 
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
-  listItemE1.textContent = "this is a task";
+  listItemE1.textContent = taskNameInput;
   tasksToDoE1.appendChild(listItemE1);
 };
 
