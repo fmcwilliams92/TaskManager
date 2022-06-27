3e4efb6e31c01c62d2100e9d105eb6de78928c24
Fredric McWilliams <fredmcwilliams92@gmail.com>
2022-06-27 16:56:50 -0500

    trying to resolve value not populating in the new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
-var taskNameInput = document.querySelector("input[name='task-name']").value;
-// left off on 4.2.6
+var taskNameInput = document.querySelector("input[name='task-name']");
 
 var createTaskHandler = function(event) {
 
@@ -9,7 +8,7 @@ var createTaskHandler = function(event) {
 
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
-  listItemE1.textContent = taskNameInput;
+  listItemE1.textContent = "this is a task";
   tasksToDoE1.appendChild(listItemE1);
 };
 
